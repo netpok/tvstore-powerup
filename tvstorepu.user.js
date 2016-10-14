da2611df1c5c489116fdff9ba2ac617220e3ffca
netpok <netpok.reg@gmail.com>
2016-10-14 12:43:06 +0200

    Add eventlisteners

--- tvstorepu.user.js
+++ tvstorepu.user.js
@@ -33,8 +33,39 @@
         GM_addStyle('.ui-widget-header .ui-icon { background-image: url(https://code.jquery.com/ui/1.12.1/themes/base/images/ui-icons_444444_256x240.png)!important;} .torrents_trakt a{outline: none;width: 28px;height: 27px;display: block;padding: 0;background: url(https://cdn-images-1.medium.com/fit/c/36/36/1*r3Oe6V2xRiOo9zlLIByGXA.png) no-repeat center;background-size: 20px;} .torrents_sj a{outline: none;width: 28px;height: 27px;display: block;padding: 0;background: url(/pic/ico_junkie.png) no-repeat center;background-size: 20px;} td.torrents_sx a, td.torrents_sx a:hover{color: #999;font-size: 10px;} td.torrents_sx {vertical-align: bottom;text-align:center;}');
         setTimeout(function () {
             $('.torrents_fajlok').after('<td class="torrents_sx" class="torrents_sx"><a title="Torrent file letöltése" href="javascript:void(0)" target="dfr">DL</a></td>');
-            $('.torrents_letolt_free>a').prop("title","qBittorrentbe küldés");
+            $('.torrents_letolt_free>a').prop("title","qBittorrentbe küldés").click(function (e) {
+                e.preventDefault();
+                var url = this.href + '&type=rss&uid=' + rss_uid + '&passkey=' + rss_passkey;
+                if ($("#applyForAll").prop("checked")) {
+                    sendToqB(url);
+                } else {
+                    $("#dlUrl").val(url);
+                    var title = $(this).closest(".torrents_epizod").prev().find("img").attr("title");
+                    if ($("#search_cat_sel_eng").find(">optgroup:first").text().indexOf(title) != -1) {
+                        title = $(this).closest("tbody").find("td:first>a").text().replace(/ - [0-9]+x[0-9]+..\[.*/, "");
+                    }
+                    $("#location").val(title.toLowerCase().replace(/[!:\(\)]/g, "").replace(/ /g, "."));
+                    $("#mediaLocation").text("/mnt/data/media");
+                    $("#dialog").dialog({
+                        buttons: {
+                            "Start download": function () {
+                                sendToqB($("#dlUrl").val());
+                                $("#dialog").dialog("close");
+                            }
+                        }
+                    });
+                }
+            });
+            $('.torrents_sx>a').click(function () {
+                this.href = $(this).closest("tr").prev().find(">td:last>a").prop('href');
+            });
             $('.torrents_nfo').before('<td class="torrents_sj"><a href="javascript:void(0)" target="_blank"></a></td><td class="torrents_trakt"><a href="javascript:void(0)" target="_blank"></a></td>');
+            $('.torrents_sj>a').click(function () {
+                this.href = "http://www.sorozatjunkie.hu/?s=" + encodeURIComponent($(this).closest(".torrents_epizod").prev().find("img").attr("title").split(" (")[0]);
+            });
+            $('.torrents_trakt>a').click(function () {
+                this.href = "https://trakt.tv/search/shows?utf8=✓&query=" + encodeURIComponent($($(this).parents("tr")[1]).find("img")[0].title.split(" (")[0]);
+            });
             $('.torrents_epizod_mufaj').attr("colspan", 2);
         }, 1000);
     }
