1504836e43d3597924e5dadde866d29117dfa97d
netpok <netpok.reg@gmail.com>
2016-10-14 12:32:52 +0200

    Update script header and add encapsulation

--- tvstorepu.user.js
+++ tvstorepu.user.js
@@ -4,14 +4,15 @@
 // @version      0.1
 // @description  Add qBittorrent links, RSS download link
 // @author       netpok
-// @match        http://www.nyaa.se/*
-// @match        https://www.nyaa.se/*
-// @require      https://code.jquery.com/jquery-3.1.1.min.js
-// @require      https://code.jquery.com/ui/1.12.1/jquery-ui.min.js
+// @match        http://tvstore.me/*
+// @match        https://tvstore.me/*
 // @require      https://cdnjs.cloudflare.com/ajax/libs/notify/0.4.2/notify.min.js
 // @require      https://cdnjs.cloudflare.com/ajax/libs/jquery-url-parser/2.3.1/purl.min.js
 // @grant        GM_addStyle
 // @grant        GM_xmlhttpRequest
 // ==/UserScript==
 /* jshint -W097 */
+'use strict';
 
+(function () {
+})();
